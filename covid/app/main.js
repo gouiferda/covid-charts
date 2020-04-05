d9eba05b932b4982193f11c4864421864ad96a22
Soufiane <soufiane.gouiferda@gmail.com>
2020-04-05 17:06:17 +0100

    updated

--- covid/app/main.js
+++ covid/app/main.js
@@ -14,7 +14,8 @@ function getListItem(text,nb,className)
     var ret = '';
     ret += '<li class="list-group-item d-flex justify-content-between align-items-center">';
     ret += text;
-    ret += '<span class="badge badge-'+className+'">';
+    //ret += '<span class="badge badge-'+className+'">';
+    ret += '<span>';
     ret += nb;
     ret += '</span>';
     ret += '</li>';
@@ -51,7 +52,7 @@ function getTodayChart() {
         countryInfoText += '<div class="row">';
         countryInfoText += '<div class="col">';
         countryInfoText += '<ul class="list-group">';
-        countryInfoText += getListItem('Total positive cases',betterNumbers(totalCases),darkClass);
+        countryInfoText += getListItem('Total cases',betterNumbers(totalCases),darkClass);
         countryInfoText += getListItem('Active cases ('+getPercentage(activeCases,totalCases)+'%)',betterNumbers(activeCases),orangeClass);
         countryInfoText += getListItem('Deaths ('+getPercentage(deathsCases,totalCases)+'%)',betterNumbers(deathsCases),redClass);
         countryInfoText += getListItem('Recovered ('+getPercentage(recoveredCases,totalCases)+'%)',betterNumbers(recoveredCases),greenClass);
